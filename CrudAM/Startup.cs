a75af9cf659609d6aa3954908ba9b2bd2f78d66c
Icaro F. Guselian <icaro.ifg@gmail.com>
2016-09-12 21:33:16 -0300

    First commit!

---
+++ CrudAM/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CrudAM.Startup))]
+namespace CrudAM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
