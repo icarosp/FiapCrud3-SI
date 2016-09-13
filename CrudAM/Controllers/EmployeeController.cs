fb0d3a45f6f762a1eb3f7337d416b5e816537485
Icaro F. Guselian <icaro.ifg@gmail.com>
2016-09-12 22:04:28 -0300

    Creating view

---
+++ CrudAM/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrudAM.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create() {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
